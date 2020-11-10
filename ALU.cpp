61886c06a7844736b3469999135954dc781a7579
Matthew Haines <matthewohaines@gmail.com>
2020-11-10 18:41:45 -0500

    push of ALU changes

--- ALU.cpp
+++ ALU.cpp
@@ -1,21 +1,28 @@
 #include "ALU.h"
+#include <bitset>
 
 
 	ALU::ALU(){}
 
 	ALU::~ALU() {}
 
-	int ALU::compute(int term1, int term2, int control)
-	{
+	int ALU::compute(int term1, int term2, std::string control){
+		if (control == "100000")
+		{
+			return add(term1, term2);
+		}
+
+		if (control == "001000")
+		{
+			return term1 + term2;
+		}
 		return 0;
 	}
 
-	int ALU::add(int term1, int term2)
-	{
+	int ALU::add(int term1, int term2){
 		return term1 + term2;
 	}
 
-	int ALU::add4(int term)
-	{
-		return term +4;
+	int ALU::add4(int term){
+		return term + 4;
 	}
--- ALU.h
+++ ALU.h
@@ -1,21 +1,17 @@
 #ifndef __ALU_H__
 #define __ALU_H__
 
+#include <bitset>
+
 
 class ALU{
 
 	public:
 		ALU();
 		~ALU();
-		int compute(int term1, int term2, int control);
+		int compute(int term1, int term2, std::string control);
 		int add(int term1, int term2);
 		int add4(int term);
-
-
-	private:
-		int inputOne;
-		int inputTwo;
-		int theControl;
 
 
 };
