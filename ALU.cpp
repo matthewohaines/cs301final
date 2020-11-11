2f6a3085604f7d5cbedca7becc93882ecf042bea
Matthew Haines <matthewohaines@gmail.com>
2020-11-11 13:13:18 -0500

    altered ALU to git what is given from control unit

--- ALU.cpp
+++ ALU.cpp
@@ -1,20 +1,35 @@
 #include "ALU.h"
-#include <bitset>
+
 
 
 	ALU::ALU(){}
 
 	ALU::~ALU() {}
 
-	int ALU::compute(int term1, int term2, std::string control){
-		if (control == "100000")
+	int ALU::compute(int term1, int term2, std::string ALUop){ // a bunch of ints representing output for ALU on diagram
+		if (ALUop == "10")  // if ALUop is 10, r-type instruciton, so either add or sub
 		{
 			return add(term1, term2);
 		}
 
-		if (control == "001000")
+		if (ALUop == "00") // if ALUop is "00", then either LW or SW
 		{
-			return term1 + term2;
+			//code
+		}
+
+		if (ALUop == "01") // if ALUop is 01, then BEQ
+		{
+			/* code */
+		}
+
+		if (ALUop == "-1") // if ALUop is -1, then jump instruction
+		{
+			/* code */
+		}
+
+		if (ALUop == "11") //if ALU op is "11", then it is ADDI instruction
+		{
+			/* code */
 		}
 		return 0;
 	}
--- ALU.h
+++ ALU.h
@@ -1,7 +1,6 @@
 #ifndef __ALU_H__
 #define __ALU_H__
 
-#include <bitset>
 
 
 class ALU{
@@ -9,7 +8,8 @@ class ALU{
 	public:
 		ALU();
 		~ALU();
-		int compute(int term1, int term2, std::string control);
+		int setALUControl(int term1, int term2, std::string ALUop);
+		int getALUControl();
 		int add(int term1, int term2);
 		int add4(int term);
 
