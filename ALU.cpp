ce4263ce5e37e93d817a43e9166cab57d5496238
Matthew Haines <matthewohaines@gmail.com>
2020-11-12 19:44:10 -0500

    changed ALU

--- ALU.cpp
+++ ALU.cpp
@@ -1,33 +1,53 @@
 #include "ALU.h"
 
 
+/* the ALU is given two terms and a control, 
+depending on what this control is, 
+a certain function will be operated on these two terms */
+#include<iostream>
+using namespace std;
 
 	ALU::ALU(){}
 
 	ALU::~ALU() {}
 
-	int ALU::compute(int term1, int term2, std::string ALUop){ // a bunch of ints representing output for ALU on diagram
-		if (ALUop == "10")  // if ALUop is 10, r-type instruciton, so either add or sub
+	int ALU::setALUControl(int term1, int term2, std::string operation){ // a bunch of ints representing output for ALU on diagram
+		if (operation == "add")  // if ALUop is 10, r-type instruciton, so either add or sub
 		{
 			return add(term1, term2);
 		}
 
-		if (ALUop == "00") // if ALUop is "00", then either LW or SW
+		if (operation== "addi") 
 		{
-			//code
+			return add(term1, term2);
 		}
 
-		if (ALUop == "01") // if ALUop is 01, then BEQ
+		if (operation == "sub") 
+		{
+			return subtract(term1, term2);
+		}
+
+		if (operation == "slt") 
+		{
+			return slt(term1, term2);
+		}
+
+		if (operation == "lw") 
 		{
 			/* code */
 		}
 
-		if (ALUop == "-1") // if ALUop is -1, then jump instruction
+		if (operation == "sw")
+		{
+
+		}
+
+		if (operation == "beq")
 		{
 			/* code */
 		}
 
-		if (ALUop == "11") //if ALU op is "11", then it is ADDI instruction
+		if (operation == "j")
 		{
 			/* code */
 		}
@@ -41,3 +61,38 @@
 	int ALU::add4(int term){
 		return term + 4;
 	}
+
+	int ALU::subtract(int term1, int term2){
+		return term1 - term2;
+	}
+
+	int ALU::slt(int term1, int term2)
+	{
+		if (term1 < term2)
+		{
+			return 1;
+		}
+		return 0;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ALU.h
+++ ALU.h
@@ -1,6 +1,8 @@
 #ifndef __ALU_H__
 #define __ALU_H__
 
+#include <iostream>
+using namespace std;
 
 
 class ALU{
@@ -8,10 +10,12 @@ class ALU{
 	public:
 		ALU();
 		~ALU();
-		int setALUControl(int term1, int term2, std::string ALUop);
+		int setALUControl(int term1, int term2, std::string operation);
 		int getALUControl();
 		int add(int term1, int term2);
 		int add4(int term);
+		int subtract(int term1, int term2);
+		int slt(int term1, int term2);
 
 
 };
